6218922e0a673eae42cd2006e6fc7a22445500dd
Geoffrey McElhanon <geoffrey@mcelhanon.com>
2020-06-23 01:11:21 -0500

    Working and cleaned up.

--- Application/EdFi.Ods.Api/Pipelines/Delete/IDeletePipelineStepsProvider.cs
+++ Application/EdFi.Ods.Api/Pipelines/Delete/IDeletePipelineStepsProvider.cs
@@ -2,16 +2,13 @@
 // Licensed to the Ed-Fi Alliance under one or more agreements.
 // The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 // See the LICENSE and NOTICES files in the project root for more information.
- 
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace EdFi.Ods.Pipelines.Common
+using EdFi.Ods.Pipelines.Common;
+
+namespace EdFi.Ods.Pipelines.Delete
 {
-    public interface IStep {}
-    
-    public interface IStep<in TContext, in TResult> : IStep
+    public interface IDeletePipelineStepsProvider
     {
-        Task ExecuteAsync(TContext context, TResult result, CancellationToken cancellationToken);
+        IStep<DeleteContext, DeleteResult>[] GetSteps<TResourceModel, TEntityModel>();
     }
 }
